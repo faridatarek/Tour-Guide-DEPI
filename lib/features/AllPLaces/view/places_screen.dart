637d3f50c32e590166a5933ebdd6fa7a85ac77fb
shimaa <shaimaafoda7@gmail.com>
2025-04-15 10:53:31 +0200

    adding features/places

---
+++ lib/features/AllPLaces/view/places_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AllPlacesScreen extends StatelessWidget {
+  const AllPlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('this is all places screen !'),),
+    );
+  }
+}
---
+++ lib/features/layout/view/navbar_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NavbarView extends StatefulWidget {
+  const NavbarView({super.key});
+
+  @override
+  State<NavbarView> createState() => _NavbarViewState();
+}
+
+class _NavbarViewState extends State<NavbarView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('this is  bottom nav bar screen  !'),),
+    );
+  }
+}
